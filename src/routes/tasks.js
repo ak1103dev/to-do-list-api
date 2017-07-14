7e6b1259ffbdfe87ebe4a1ac12d2d7ae5bbefb15
ak1103dev <ak.apichan1103@gmail.com>
2017-07-14 17:31:08 +0700

    :sparkles: can add task

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -12,4 +12,22 @@ router.get('/', (req, res) => {
   return res.status(401).send({ message: 'Unauthorized User' });
 });
 
+router.post('/', (req, res) => {
+  if (!req.user) {
+    return res.status(401).send({ message: 'Unauthorized User' });
+  }
+  const bodySchema = {
+    text: Joi.string().required(),
+    status: Joi.string().required(),
+  }
+  const { error, value } = Joi.validate(req.body, bodySchema);
+  if (error) {
+    return res.status(400).send(error);
+  }
+  const { text, status } = value;
+  return new Task({ text, status, userId: req.user._id }).save()
+    .then((task) => res.send(task))
+    .catch((err) => res.status(500).send({ error: err }));
+});
+
 module.exports = router;
